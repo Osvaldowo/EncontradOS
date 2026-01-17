be654098423f9e46305a084ab39fc2bcd17d6d58
IanDCaballeroL <ian.dcaballerol@gmail.com>
2026-01-17 11:53:08 -0600

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "encontrados.firebaseapp.com",
-  projectId: "encontrados",
-  storageBucket: "encontrados.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abc123"
+  apiKey: "AIzaSyAM6oQYQrO1DP1nVQ8shzj58o-MUqtJHmM",
+  authDomain: "encontrados-156c7.firebaseapp.com",
+  projectId: "encontrados-156c7",
+  storageBucket: "encontrados-156c7.firebasestorage.app",
+  messagingSenderId: "989938386656",
+  appId: "1:989938386656:web:aea01e2cd28745641f3285",
+  measurementId: "G-74DHPZMGH8"
+
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
